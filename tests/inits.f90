c2e42d8394d5261241e1146b646ee0aa5c702840
Edward <edward_yang_125@hotmail.com>
2023-10-25 20:43:21 +1100

    merge size and 0 tests

--- tests/inits.f90
+++ tests/inits.f90
@@ -36,126 +36,66 @@ contains
 
         write(output_unit, "(4x, A)") "p%ntotal == 0"
         write(output_unit, "(4x, A)") pass_or_fail(p%ntotal == 0)
-        write(output_unit, "(4x, A)")  "p%ntotal == 0"
-        write(output_unit, "(4x, A)")  pass_or_fail(p%ntotal == 0)
         write(output_unit, "(4x, A)")  "p%maxn == maxn"
         write(output_unit, "(4x, A)")  pass_or_fail(p%maxn == maxn)
-        write(output_unit, "(4x, A)")  "size(p%x) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%x) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%x == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%x == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%y) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%y) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%y == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%y == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%z) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%z) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%z == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%z == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%vx) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%vx) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%vx == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%vx == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%vy) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%vy) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%vy == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%vy == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%vz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%vz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%vz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%vz == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%sigxx) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigxx) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%sigxx == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%sigxx == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%sigyy) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigyy) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%sigyy == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%sigyy == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%sigzz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigzz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%sigzz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%sigzz == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%sigxy) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigxy) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%sigxy == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%sigxy == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%sigxz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigxz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%sigxz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%sigxz == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%sigyz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigyz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%sigyz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%sigyz == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%mass) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%mass) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%mass == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%mass == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%rho) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%rho) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%rho == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%rho == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%ax) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%ax) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%ax == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%ax == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%ay) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%ay) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%ay == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%ay == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%az) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%az) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%az == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%az == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%dx) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%dx) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%dx == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%dx == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%dy) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%dy) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%dy == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%dy == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%dz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%dz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%dz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%dz == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%epsxx) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsxx) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%epsxx == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%epsxx == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%epsyy) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsyy) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%epsyy == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%epsyy == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%epszz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epszz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%epszz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%epszz == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%epsxy) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsxy) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%epsxy == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%epsxy == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%epsxz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsxz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%epsxz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%epsxz == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%epsyz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsyz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%epsyz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%epsyz == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%omegaxy) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%omegaxy) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%omegaxy == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%omegaxy == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%omegaxz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%omegaxz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%omegaxz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%omegaxz == 0._f))
-        write(output_unit, "(4x, A)")  "size(p%omegayz) == maxn"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(p%omegayz) == maxn)
-        write(output_unit, "(4x, A)")  "all(p%omegayz == 0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(p%omegayz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%x) == maxn .and. all(p%x == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%x) == maxn .and. all(p%x == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%y) == maxn .and. all(p%y == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%y) == maxn .and. all(p%y == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%z) == maxn .and. all(p%z == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%z) == maxn .and. all(p%z == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%vx) == maxn .and. all(p%vx == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%vx) == maxn .and. all(p%vx == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%vy) == maxn .and. all(p%vy == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%vy) == maxn .and. all(p%vy == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%vz) == maxn .and. all(p%vz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%vz) == maxn .and. all(p%vz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%sigxx) == maxn .and. all(p%sigxx == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigxx) == maxn .and. all(p%sigxx == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%sigyy) == maxn .and. all(p%sigyy == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigyy) == maxn .and. all(p%sigyy == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%sigzz) == maxn .and. all(p%sigzz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigzz) == maxn .and. all(p%sigzz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%sigxy) == maxn .and. all(p%sigxy == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigxy) == maxn .and. all(p%sigxy == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%sigxz) == maxn .and. all(p%sigxz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigxz) == maxn .and. all(p%sigxz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%sigyz) == maxn .and. all(p%sigyz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%sigyz) == maxn .and. all(p%sigyz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%mass) == maxn .and. all(p%mass == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%mass) == maxn .and. all(p%mass == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%rho) == maxn .and. all(p%rho == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%rho) == maxn .and. all(p%rho == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%ax) == maxn .and. all(p%ax == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%ax) == maxn .and. all(p%ax == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%ay) == maxn .and. all(p%ay == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%ay) == maxn .and. all(p%ay == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%az) == maxn .and. all(p%az == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%az) == maxn .and. all(p%az == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%dx) == maxn .and. all(p%dx == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%dx) == maxn .and. all(p%dx == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%dy) == maxn .and. all(p%dy == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%dy) == maxn .and. all(p%dy == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%dz) == maxn .and. all(p%dz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%dz) == maxn .and. all(p%dz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%epsxx) == maxn .and. all(p%epsxx == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsxx) == maxn .and. all(p%epsxx == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%epsyy) == maxn .and. all(p%epsyy == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsyy) == maxn .and. all(p%epsyy == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%epszz) == maxn .and. all(p%epszz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epszz) == maxn .and. all(p%epszz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%epsxy) == maxn .and. all(p%epsxy == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsxy) == maxn .and. all(p%epsxy == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%epsxz) == maxn .and. all(p%epsxz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsxz) == maxn .and. all(p%epsxz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%epsyz) == maxn .and. all(p%epsyz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%epsyz) == maxn .and. all(p%epsyz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%omegaxy) == maxn .and. all(p%omegaxy == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%omegaxy) == maxn .and. all(p%omegaxy == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%omegaxz) == maxn .and. all(p%omegaxz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%omegaxz) == maxn .and. all(p%omegaxz == 0._f))
+        write(output_unit, "(4x, A)")  "size(p%omegayz) == maxn .and. all(p%omegayz == 0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(p%omegayz) == maxn .and. all(p%omegayz == 0._f))
 
     end subroutine test_particle_init
 
@@ -198,46 +138,26 @@ contains
         write(output_unit, "(4x, A)")  pass_or_fail(all(g%maxgrid == maxgrid+dcell*n_buffer_cells))
         write(output_unit, "(4x, A)")  "all(g%ngrid == correct_ngrid) "
         write(output_unit, "(4x, A)")  pass_or_fail(all(g%ngrid == correct_ngrid))
-        write(output_unit, "(4x, A)")  "size(g%mass) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%mass) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%mass==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%mass==0._f))
-        write(output_unit, "(4x, A)")  "size(g%momentumx) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%momentumx) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%momentumx==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%momentumx==0._f))
-        write(output_unit, "(4x, A)")  "size(g%momentumy) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%momentumy) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%momentumy==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%momentumy==0._f))
-        write(output_unit, "(4x, A)")  "size(g%momentumz) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%momentumz) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%momentumz==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%momentumz==0._f))
-        write(output_unit, "(4x, A)")  "size(g%forcex) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%forcex) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%forcex==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%forcex==0._f))
-        write(output_unit, "(4x, A)")  "size(g%forcey) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%forcey) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%forcey==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%forcey==0._f))
-        write(output_unit, "(4x, A)")  "size(g%forcez) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%forcez) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%forcez==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%forcez==0._f))
-        write(output_unit, "(4x, A)")  "size(g%vx) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%vx) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%vx==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%vx==0._f))
-        write(output_unit, "(4x, A)")  "size(g%vy) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%vy) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%vy==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%vy==0._f))
-        write(output_unit, "(4x, A)")  "size(g%vz) == ncells"
-        write(output_unit, "(4x, A)")  pass_or_fail(size(g%vz) == ncells)
-        write(output_unit, "(4x, A)")  "all(g%vz==0._f)"
-        write(output_unit, "(4x, A)")  pass_or_fail(all(g%vz==0._f))
+        write(output_unit, "(4x, A)")  "size(g%mass) == ncells .and. all(g%mass==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%mass) == ncells .and. all(g%mass==0._f))
+        write(output_unit, "(4x, A)")  "size(g%momentumx) == ncells .and. all(g%momentumx==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%momentumx) == ncells .and. all(g%momentumx==0._f))
+        write(output_unit, "(4x, A)")  "size(g%momentumy) == ncells .and. all(g%momentumy==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%momentumy) == ncells .and. all(g%momentumy==0._f))
+        write(output_unit, "(4x, A)")  "size(g%momentumz) == ncells .and. all(g%momentumz==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%momentumz) == ncells .and. all(g%momentumz==0._f))
+        write(output_unit, "(4x, A)")  "size(g%forcex) == ncells .and. all(g%forcex==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%forcex) == ncells .and. all(g%forcex==0._f))
+        write(output_unit, "(4x, A)")  "size(g%forcey) == ncells .and. all(g%forcey==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%forcey) == ncells .and. all(g%forcey==0._f))
+        write(output_unit, "(4x, A)")  "size(g%forcez) == ncells .and. all(g%forcez==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%forcez) == ncells .and. all(g%forcez==0._f))
+        write(output_unit, "(4x, A)")  "size(g%vx) == ncells .and. all(g%vx==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%vx) == ncells .and. all(g%vx==0._f))
+        write(output_unit, "(4x, A)")  "size(g%vy) == ncells .and. all(g%vy==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%vy) == ncells .and. all(g%vy==0._f))
+        write(output_unit, "(4x, A)")  "size(g%vz) == ncells .and. all(g%vz==0._f)"
+        write(output_unit, "(4x, A)")  pass_or_fail(size(g%vz) == ncells .and. all(g%vz==0._f))
 
     end subroutine test_grid_init
 
