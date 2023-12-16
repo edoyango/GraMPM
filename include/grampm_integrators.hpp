1560100419f590b28f258c22d479fccd35c8b9b0
Edward <edward_yang_125@hotmail.com>
2023-12-16 12:04:10 +1100

    Revert "omp base case master thread only"
    This reverts commit c442f49e2c92cc67e53fb023f6fb4aaa072762ae.

--- include/grampm_integrators.hpp
+++ include/grampm_integrators.hpp
@@ -9,86 +9,66 @@ namespace GraMPM {
         template<typename F>
         void MUSL(GraMPM::particle_system<F> &p, const F &dt, const int &max_timestep, 
             const int &print_timestep_interval, const int &save_timestep_interval) {
+            
+            for (int itimestep = 1; itimestep < max_timestep+1; ++itimestep) {
 
-            #pragma omp parallel default(shared)
-            {
-                for (int itimestep = 1; itimestep < max_timestep+1; ++itimestep) {
-
-                    // print to terminal
-                    #pragma omp master
-                    if (itimestep % print_timestep_interval == 0) {
-                        std::cout << itimestep << ' ' << dt*itimestep << '\n';
-                    }
-
-                    // get which cell each particle is located within
-                    #pragma omp master
-                    p.update_particle_to_cell_map();
-
-                    /* get all neighbour nodes of each particle, calculate dx between particles and adjacent nodes, and
-                    update kernel and kernel gradient values. */ 
-                    #pragma omp master
-                    p.map_particles_to_grid();
-
-                    // map particles' mass to nodes
-                    #pragma omp master
-                    p.map_mass_to_grid();
-
-                    // map particles' momentum to nodes
-                    #pragma omp master
-                    p.map_momentum_to_grid();
-
-                    // apply user-defined momentum boundary conditions to grid
-                    #pragma omp master
-                    p.background_grid.apply_momentum_boundary_conditions(itimestep, dt);
-
-                    // map particles' force to nodes
-                    #pragma omp master
-                    p.map_force_to_grid();
-
-                    // apply user-defined force boundary conditions to grid
-                    #pragma omp master
-                    p.background_grid.apply_force_boundary_conditions(itimestep, dt);
-
-                    // update nodal momentums
-                    p.background_grid.update_momentum(dt);
-
-                    // map nodal forces to particle accelerations
-                    #pragma omp master
-                    p.map_acceleration_to_particles();
-
-                    // update particles' velocities with calculated accelerations
-                    #pragma omp master
-                    p.update_velocity(dt);
-                    // update particles' position
-                    #pragma omp master
-                    p.update_position(dt);
-
-                    // map particles' momentum to nodes, in preparation for updating stress
-                    #pragma omp master
-                    p.map_momentum_to_grid();
-
-                    // apply user-defined momentum boundary conditions to grid
-                    #pragma omp master
-                    p.background_grid.apply_momentum_boundary_conditions(itimestep, dt);
-
-                    // map nodal velocities to particle strain/spin rates
-                    #pragma omp master
-                    p.map_strainrate_to_particles();
-
-                    // update particles' density
-                    #pragma omp master
-                    p.update_density(dt);
-
-                    // update particles' stress
-                    #pragma omp master
-                    p.update_stress(dt);
-
-                    #pragma omp master
-                    if (itimestep % save_timestep_interval == 0) {
-                        p.save_to_file("p_", itimestep);
-                    }
-
+                // print to terminal
+                if (itimestep % print_timestep_interval == 0) {
+                    std::cout << itimestep << ' ' << dt*itimestep << '\n';
                 }
+
+                // get which cell each particle is located within
+                p.update_particle_to_cell_map();
+
+                /* get all neighbour nodes of each particle, calculate dx between particles and adjacent nodes, and
+                update kernel and kernel gradient values. */ 
+                p.map_particles_to_grid();
+
+                // map particles' mass to nodes
+                p.map_mass_to_grid();
+
+                // map particles' momentum to nodes
+                p.map_momentum_to_grid();
+
+                // apply user-defined momentum boundary conditions to grid
+                p.background_grid.apply_momentum_boundary_conditions(itimestep, dt);
+
+                // map particles' force to nodes
+                p.map_force_to_grid();
+
+                // apply user-defined force boundary conditions to grid
+                p.background_grid.apply_force_boundary_conditions(itimestep, dt);
+
+                // update nodal momentums
+                p.background_grid.update_momentum(dt);
+
+                // map nodal forces to particle accelerations
+                p.map_acceleration_to_particles();
+
+                // update particles' velocities with calculated accelerations
+                p.update_velocity(dt);
+                // update particles' position
+                p.update_position(dt);
+
+                // map particles' momentum to nodes, in preparation for updating stress
+                p.map_momentum_to_grid();
+
+                // apply user-defined momentum boundary conditions to grid
+                p.background_grid.apply_momentum_boundary_conditions(itimestep, dt);
+
+                // map nodal velocities to particle strain/spin rates
+                p.map_strainrate_to_particles();
+
+                // update particles' density
+                p.update_density(dt);
+
+                // update particles' stress
+                p.update_stress(dt);
+
+                if (itimestep % save_timestep_interval == 0) {
+                    p.save_to_file("p_", itimestep);
+                }
+
             }
         }
     }
