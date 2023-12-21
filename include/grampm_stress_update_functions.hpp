27595e700170202fb5987520cd7733b0e0b74222
Edward <edward_yang_125@hotmail.com>
2023-12-22 09:42:48 +1100

    direct member reference in hookes law

--- include/grampm_stress_update_functions.hpp
+++ include/grampm_stress_update_functions.hpp
@@ -9,54 +9,29 @@ namespace GraMPM {
         template<typename F>
         void hookes_law(GraMPM::particle_system<F> &self, const F &dt) {
 
-            const F E {self.E()}, v {self.v()};
-            const int size {static_cast<int>(self.size())};
-            std::vector<F> &sigmaxx {*(self.sigmaxx())}, &sigmayy {*(self.sigmayy())}, 
-                &sigmazz {*(self.sigmazz())}, &sigmaxy {*(self.sigmaxy())}, 
-                &sigmaxz {*(self.sigmaxz())}, &sigmayz {*(self.sigmayz())};
-            std::vector<F> &strainratexx {*(self.strainratexx())}, &strainrateyy {*(self.strainrateyy())}, 
-                &strainratezz {*(self.strainratezz())}, &strainratexy {*(self.strainratexy())}, 
-                &strainratexz {*(self.strainratexz())}, &strainrateyz {*(self.strainrateyz())};
-            std::vector<F> &spinratexy {*(self.spinratexy())}, &spinratexz {*(self.spinratexz())},
-                &spinrateyz {*(self.spinrateyz())};
-
-            const F D0 {E/((1.+v)*(1.-2.*v))};
-
-            std::vector<F> dsigmaxx(size), dsigmayy(size), dsigmazz(size), dsigmaxy(size), dsigmaxz(size), 
-                dsigmayz(size);
+            const F D0 {self.m_E/((1.+self.m_v)*(1.-2.*self.m_v))};
 
             // DE*dstrain
             #pragma omp for
-            for (int i = 0; i < size; ++i) {
-                dsigmaxx[i] = D0*((1.-v)*strainratexx[i] + v*strainrateyy[i] + v*strainratezz[i]);
-                dsigmayy[i] = D0*(v*strainratexx[i] + (1.-v)*strainrateyy[i] + v*strainratezz[i]);
-                dsigmazz[i] = D0*(v*strainratexx[i] + v*strainrateyy[i] + (1.-v)*strainratezz[i]);
-                dsigmaxy[i] = D0*strainratexy[i]*(1.-2.*v);
-                dsigmaxz[i] = D0*strainratexz[i]*(1.-2.*v);
-                dsigmayz[i] = D0*strainrateyz[i]*(1.-2.*v);
-            // }
-            
-            // jaumann stress rate
-            // for (int i = 0; i < size; ++i) {
-                dsigmaxx[i] -= 2.*(spinratexy[i]*sigmaxy[i] + spinratexz[i]*sigmaxz[i]);
-                dsigmayy[i] -= 2.*(-spinratexy[i]*sigmaxy[i] + spinrateyz[i]*sigmayz[i]);
-                dsigmazz[i] += 2.*(spinratexz[i]*sigmaxz[i] + spinrateyz[i]*sigmayz[i]);
-                dsigmaxy[i] += sigmaxx[i]*spinratexy[i] - sigmaxz[i]*spinrateyz[i] -
-                    spinratexy[i]*sigmayy[i] - spinratexz[i]*sigmayz[i];
-                dsigmaxz[i] += sigmaxx[i]*spinratexz[i] + sigmaxy[i]*spinrateyz[i] -
-                    spinratexy[i]*sigmayz[i] - spinratexz[i]*sigmazz[i];
-                dsigmayz[i] += sigmaxy[i]*spinratexz[i] + sigmayy[i]*spinrateyz[i] +
-                    spinratexy[i]*sigmaxz[i] - spinrateyz[i]*sigmazz[i];
-            // }
+            for (int i = 0; i < self.m_size; ++i) {
+                // jaumann stress rate (applied first, as it depends on current stress state)
+                self.m_sigmaxx[i] -= dt*2.*(self.m_spinratexy[i]*self.m_sigmaxy[i] + self.m_spinratexz[i]*self.m_sigmaxz[i]);
+                self.m_sigmayy[i] -= dt*2.*(-self.m_spinratexy[i]*self.m_sigmaxy[i] + self.m_spinrateyz[i]*self.m_sigmayz[i]);
+                self.m_sigmazz[i] += dt*2.*(self.m_spinratexz[i]*self.m_sigmaxz[i] + self.m_spinrateyz[i]*self.m_sigmayz[i]);
+                self.m_sigmaxy[i] += dt*(self.m_sigmaxx[i]*self.m_spinratexy[i] - self.m_sigmaxz[i]*self.m_spinrateyz[i] -
+                    self.m_spinratexy[i]*self.m_sigmayy[i] - self.m_spinratexz[i]*self.m_sigmayz[i]);
+                self.m_sigmaxz[i] += dt*(self.m_sigmaxx[i]*self.m_spinratexz[i] + self.m_sigmaxy[i]*self.m_spinrateyz[i] -
+                    self.m_spinratexy[i]*self.m_sigmayz[i] - self.m_spinratexz[i]*self.m_sigmazz[i]);
+                self.m_sigmayz[i] += dt*(self.m_sigmaxy[i]*self.m_spinratexz[i] + self.m_sigmayy[i]*self.m_spinrateyz[i] +
+                    self.m_spinratexy[i]*self.m_sigmaxz[i] - self.m_spinrateyz[i]*self.m_sigmazz[i]);
 
-            // update original stress states
-            // for (int i = 0; i < size; ++i) {
-                sigmaxx[i] += dt*dsigmaxx[i];
-                sigmayy[i] += dt*dsigmayy[i];
-                sigmazz[i] += dt*dsigmazz[i];
-                sigmaxy[i] += dt*dsigmaxy[i];
-                sigmaxz[i] += dt*dsigmaxz[i];
-                sigmayz[i] += dt*dsigmayz[i];
+                // update stress state with elastic increment
+                self.m_sigmaxx[i] += dt*D0*((1.-self.m_v)*self.m_strainratexx[i] + self.m_v*self.m_strainrateyy[i] + self.m_v*self.m_strainratezz[i]);
+                self.m_sigmayy[i] += dt*D0*(self.m_v*self.m_strainratexx[i] + (1.-self.m_v)*self.m_strainrateyy[i] + self.m_v*self.m_strainratezz[i]);
+                self.m_sigmazz[i] += dt*D0*(self.m_v*self.m_strainratexx[i] + self.m_v*self.m_strainrateyy[i] + (1.-self.m_v)*self.m_strainratezz[i]);
+                self.m_sigmaxy[i] += dt*D0*self.m_strainratexy[i]*(1.-2.*self.m_v);
+                self.m_sigmaxz[i] += dt*D0*self.m_strainratexz[i]*(1.-2.*self.m_v);
+                self.m_sigmayz[i] += dt*D0*self.m_strainrateyz[i]*(1.-2.*self.m_v);
             }
 
         }
